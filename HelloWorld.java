2d55363e68a3b9f1e5682fb23934e1384256842c
Astral Gnome <reedkeenan@gmail.com>
2021-10-21 17:48:01 -0700

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * HelloWorld
+ */
+
+// First, begin typing "class" and then select IntelliCode prompt
+public class HelloWorld {
+  
+  // Then begin typing "main" and select Intellicode prompt
+  public static void main(String[] args) {
+    // Then begin typing "sysout" and select IntelliCode prompt
+    System.out.println("Hello World");
+    
+}
+  
+}
